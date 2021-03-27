80c3ef4b924ceb5b5030e04ddd703a7abd7be00b
Jonathan Laferen <60574415+jonathanmarp@users.noreply.github.com>
2021-03-27 14:17:17 +0700

    Update
    declare let process: any;
    // just exporting variable pathFile
    let isWin: boolean = process.platform === "win32";
    export let pathFile: string = isWin == true ? "jgwjs/bin/JGW.exe" : "jgwsjs/bin/JGW";

--- srcTS/pathFile.ts
+++ srcTS/pathFile.ts
@@ -1,4 +1,4 @@
 declare let process: any;
 // just exporting variable pathFile
 let isWin: boolean = process.platform === "win32";
-export let pathFile: string = isWin == true ? "bin/JGW.exe" : "bin/JGW";
+export let pathFile: string = isWin == true ? "jgwjs/bin/JGW.exe" : "jgwsjs/bin/JGW";
