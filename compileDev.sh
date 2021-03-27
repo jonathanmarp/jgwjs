e1213abb5d771d4d83a68fc7e780ff1274124f7f
Jonathan Laferen <60574415+jonathanmarp@users.noreply.github.com>
2021-03-27 14:37:11 +0700

    Add files via upload

--- compileDev.sh
+++ compileDev.sh
@@ -1,17 +1,12 @@
 # compile sound to library
-cd sound
-g++ -c sound.cpp -o libsound.o
-ar rcs libsound.a libsound.o
-mv libsound.o bin
-mv libsound.a bin
-cd ..
+g++ -c jgwjs/sound/sound.cpp -o jgwjs/sound/libsound.o
+ar rcs jgwjs/sound/libsound.a jgwjs/sound/libsound.o
+mv jgwjs/sound/libsound.o jgwjs/sound/bin
+mv jgwjs/sound/libsound.a jgwjs/sound/bin
 
 # compile to executable
-cd src
-g++ -D_DEV -c jgw.cpp
-g++ -o JGW jgw.o -O3 -foptimize-sibling-calls -L../sound/bin -lsound `sdl2-config --cflags --libs`
-mv jgw.o bin
-mv JGW ..
-cd ..
+g++ -D_DEV -c jgwjs/src/jgw.cpp -o jgwjs/src/jgw.o
+g++ -o jgwjs/src/JGW jgwjs/src/jgw.o -O3 -foptimize-sibling-calls -Ljgwjs/sound/bin -lsound `sdl2-config --cflags --libs`
+mv jgwjs/src/jgw.o jgwjs/src/bin
 
-mv JGW bin
+mv jgwjs/src/JGW jgwjs/bin
