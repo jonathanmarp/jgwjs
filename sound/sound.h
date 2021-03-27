3f38c982a725cfcef6fa00eb57ad109a0750954e
Jonathan Laferen <60574415+jonathanmarp@users.noreply.github.com>
2021-03-27 10:58:20 +0700

    Update: For Support To Linux Or Windows
    ```cpp
    #ifndef SOUND_H
    #define SOUND_H
    
    #ifdef _WIN32
        #include <Windows.h>
    #else
        #include <SDL2/SDL.h>
        #include <SDL2/SDL_audio.h>
        #include <queue>
        #include <cmath>
    
        struct BeepObject
        {
            double freq;
            int samplesLeft;
        };
    
        class Beeper {
        	private:
            	double v;
            	std::queue<BeepObject> beeps;
        	public:
            	Beeper();
            	~Beeper();
            	void beep(double freq, int duration);
            	void generateSamples(Sint16 *stream, int length);
            	void wait();
        };
    
        void audio_callback(void*, Uint8*, int);
    #endif
    
    void tone(double Hz_S, int duration_S);
    
    #endif
    ```

--- sound/sound.h
+++ sound/sound.h
@@ -1,30 +1,34 @@
 #ifndef SOUND_H
 #define SOUND_H
 
-#include <SDL2/SDL.h>
-#include <SDL2/SDL_audio.h>
-#include <queue>
-#include <cmath>
+#ifdef _WIN32
+    #include <Windows.h>
+#else
+    #include <SDL2/SDL.h>
+    #include <SDL2/SDL_audio.h>
+    #include <queue>
+    #include <cmath>
 
-struct BeepObject
-{
-    double freq;
-    int samplesLeft;
-};
+    struct BeepObject
+    {
+        double freq;
+        int samplesLeft;
+    };
 
-class Beeper {
-	private:
-    	double v;
-    	std::queue<BeepObject> beeps;
-	public:
-    	Beeper();
-    	~Beeper();
-    	void beep(double freq, int duration);
-    	void generateSamples(Sint16 *stream, int length);
-    	void wait();
-};
+    class Beeper {
+    	private:
+        	double v;
+        	std::queue<BeepObject> beeps;
+    	public:
+        	Beeper();
+        	~Beeper();
+        	void beep(double freq, int duration);
+        	void generateSamples(Sint16 *stream, int length);
+        	void wait();
+    };
 
-void audio_callback(void*, Uint8*, int);
+    void audio_callback(void*, Uint8*, int);
+#endif
 
 void tone(double Hz_S, int duration_S);
 
