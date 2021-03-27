96c08f1a8b205f163f83dd2d2061a952b93abdda
Jonathan Laferen <60574415+jonathanmarp@users.noreply.github.com>
2021-03-27 14:16:41 +0700

    Update
    "use strict";
    exports.__esModule = true;
    exports.pathFile = void 0;
    // just exporting variable pathFile
    var isWin = process.platform === "win32";
    exports.pathFile = isWin == true ? "jgwjs/bin/JGW.exe" : "jgwjs/bin/JGW";

--- srcJS/pathFile.js
+++ srcJS/pathFile.js
@@ -3,4 +3,4 @@ exports.__esModule = true;
 exports.pathFile = void 0;
 // just exporting variable pathFile
 var isWin = process.platform === "win32";
-exports.pathFile = isWin == true ? "bin/JGW.exe" : "bin/JGW";
+exports.pathFile = isWin == true ? "jgwjs/bin/JGW.exe" : "jgwjs/bin/JGW";
