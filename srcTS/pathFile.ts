b50ad51eff09ca77a034c711d3a0ad91b0a31bfc
Jonathan Laferen <60574415+jonathanmarp@users.noreply.github.com>
2021-04-04 06:36:21 +0700

    Delete srcTS directory

--- srcTS/pathFile.ts
+++
@@ -1,4 +0,0 @@
-declare let process: any;
-// just exporting variable pathFile
-let isWin: boolean = process.platform === "win32";
-export let pathFile: string = isWin == true ? "jgwjs/bin/JGW.exe" : "jgwsjs/bin/JGW";
--- srcTS/startExec.ts
+++
@@ -1,7 +0,0 @@
-declare function require(name:string);
-const execSync: any = require('child_process').execSync; // library for run shell in 
-
-// exporting function startExec with parameter cmd (string)
-export function startExec(cmd: string) {
-	return execSync(cmd);
-}
