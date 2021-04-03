70cbf345fb5cd8e0a5e28611a79839823dd71d23
Jonathan Laferen <60574415+jonathanmarp@users.noreply.github.com>
2021-04-04 06:34:58 +0700

    Delete srcJS directory

--- srcJS/pathFile.js
+++
@@ -1,6 +0,0 @@
-"use strict";
-exports.__esModule = true;
-exports.pathFile = void 0;
-// just exporting variable pathFile
-var isWin = process.platform === "win32";
-exports.pathFile = isWin == true ? "jgwjs/bin/JGW.exe" : "jgwjs/bin/JGW";
